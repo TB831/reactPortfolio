c26e86ede96603eb6cb9c30fb33b929a05b93696
TB831 <druu831@gmail.com>
2019-03-06 11:43:31 -0800

    added languages row to technology

--- src/components/Technology.js
+++ src/components/Technology.js
@@ -3,9 +3,47 @@ import React from 'react';
 const Technology = () => {
   return (
     <div className="container">
-      <div className="row">
-        <div>
-          <h3>Languages</h3>
+      <div className="row languages">
+        <h3>Languages</h3>
+        <div className="col s3 m2">
+          <div className="card z-depth-3">
+            <div className="card-image">
+              <img src="http://joelcox.io/scripts/logos/es6-logo.png" alt="es6"></img>
+              <p className="center"><strong>Es6</strong></p>
+            </div>
+          </div>
+        </div>
+        <div className="col s3 m2">
+          <div className="card z-depth-3">
+            <div className="card-image">
+              <img src="http://joelcox.io/scripts/logos/javascript-logo.png" alt="JavaScript"></img>
+              <p className="center"><strong>JavaScript</strong></p>
+            </div>
+          </div>
+        </div>
+        <div className="col s3 m2">
+          <div className="card z-depth-3">
+            <div className="card-image">
+              <img src="https://raw.githubusercontent.com/remojansen/logo.ts/master/ts.png" alt="TypeScript"></img>
+              <p className="center"><strong>TypeScript</strong></p>
+            </div>
+          </div>
+        </div>
+        <div className="col s3 m2">
+          <div className="card z-depth-3">
+            <div className="card-image">
+              <img src="https://cdn.iconscout.com/icon/free/png-256/html5-10-569380.png" alt="HTML5"></img>
+              <p className="center"><strong>HTML5</strong></p>
+            </div>
+          </div>
+        </div>
+        <div className="col s3 m2">
+          <div className="card z-depth-3">
+            <div className="card-image">
+              <img src="http://a1449.phobos.apple.com/us/r30/Purple4/v4/e8/20/fd/e820fded-8a78-06ac-79d0-f1d140346976/mzl.huoealqj.png" alt="Python"></img>
+              <p className="center"><strong>Python</strong></p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
