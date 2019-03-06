f845a2d959264ff6a2d7f9effeecfeec8eabcc92
TB831 <druu831@gmail.com>
2019-03-05 21:03:31 -0800

    added boilerplates for future components: About. Contact. Home. Projects. Setup routes for corresponding components

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
   return (
     <ul id="nav-mobile" className="sidenav sidenav-fixed blue">
-     <li><a href="/">About</a></li>
-     <li><a href="/">About</a></li>
-     <li><a href="/">About</a></li>
-     <li><a href="/">About</a></li>
+      <li><NavLink to="/">Home</NavLink></li>
+      <li><NavLink to="/about">About</NavLink></li>
+      <li><NavLink to="/projects">Projects</NavLink></li>
+      <li><NavLink to="/contact">Contact</NavLink></li>
     </ul>
   );
 }
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-
 import NavBar from '../components/NavBar';
+import Home from '../components/Home';
+import Projects from '../components/Projects';
+import Contact from '../components/Contact';
+import About from '../components/About';
 
 class App extends Component {
   render() {
@@ -9,6 +12,12 @@ class App extends Component {
       <BrowserRouter>
         <div>
           <NavBar />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/projects" component={Projects} />
+            <Route path="/contact" component={Contact} /> 
+          </Switch>
         </div>
       </BrowserRouter>
     );
